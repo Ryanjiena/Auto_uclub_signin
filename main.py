4a394bbc063734b0db944317e27ed50d40513c4b
Ryanjiena <ryanjiena@foxmail.com>
2020-12-30 15:47:14 +0800

    :pencil2: Fix typos.

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ TELEGRAMBOT_CHATID = os.environ["TELEGRAMBOT_CHATID"]
 username = ""
 password = ""
 
+if(username == "", password == ""):
+    username = input("账号:")
+    password = input("密码:")
+
 HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66",
