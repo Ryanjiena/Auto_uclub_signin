f68b5106cb3155a831875d0c38ca15b1df070b06
Ryanjiena <ryanjiena@foxmail.com>
2020-12-30 12:21:30 +0800

    :sparkles: 添加 Qmsg 酱推送.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 
 USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
+QMSG_KEY = os.environ["QMSG_KEY"]
 TELEGRAMBOT_TOKEN = os.environ["TELEGRAMBOT_TOKEN"]
 TELEGRAMBOT_CHATID = os.environ["TELEGRAMBOT_CHATID"]
 
@@ -51,7 +52,15 @@ def signin(session):
 
 
 def telegrambot_sendmessage(bot_message):
-    send_message = 'https://api.telegram.org/bot' + TELEGRAMBOT_TOKEN + '/sendMessage?chat_id=' + TELEGRAMBOT_CHATID + '&parse_mode=Markdown&text=' + bot_message
+    send_message = 'https://api.telegram.org/bot' + TELEGRAMBOT_TOKEN + \
+        '/sendMessage?chat_id=' + TELEGRAMBOT_CHATID + \
+        '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_message)
+    print(response.json())
+
+
+def qmsg_sendmessage(bot_message):
+    send_message = 'https://qmsg.zendee.cn/send/' + QMSG_KEY + '?msg=' + bot_message
     response = requests.get(send_message)
     print(response.json())
 
@@ -60,22 +69,26 @@ if __name__ == '__main__':
     if not USERNAME or not PASSWORD:
         print("你没有添加账户密码")
         exit(1)
-    if not TELEGRAMBOT_TOKEN or not TELEGRAMBOT_CHATID:
-        print("你没有配置 TelegramBot")
-        exit(1)
     s = login()
-    v_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
-    message0 = "UCloud Uclub 社区今日打卡情况：\n\n - 打卡时间：" + v_time + "\n - 打卡用户：" + USERNAME + "\n"
+    v_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    title = "UCloud Uclub 社区今日打卡情况：\n\n"
+    message = " - 打卡时间：" + v_time + "\n - 打卡用户：" + USERNAME + "\n"
     if not s:
-        message1 = " - 打卡日志：登陆失败，请检查你的登录信息是否准确!" + "\n"
-        message = message0 + message1
-        telegrambot_sendmessage(message)
+        message = message + " - 打卡日志：登陆失败，请检查你的登录信息是否准确!" + "\n"
+        message0 = title + message
+        if TELEGRAMBOT_TOKEN and TELEGRAMBOT_CHATID:
+            telegrambot_sendmessage(message0)
+        if QMSG_KEY:
+            qmsg_sendmessage(message0)
         exit(1)
     else:
-        message1 = " - 打卡前积分为: %d"%(getCredit(s)) + "\n"
+        message = message + " - 打卡前积分为: %d" % (getCredit(s)) + "\n"
         print('登陆成功，您目前的积分为: %d' % getCredit(s))
         signin(s)
-        message2 = " - 打卡后积分为: %d"%(getCredit(s)) + "\n"
+        message = message + " - 打卡后积分为: %d" % (getCredit(s)) + "\n"
         print('签到完成，您目前的积分为: %d' % getCredit(s))
-        message = message0 + message1 + message2
-        telegrambot_sendmessage(message)
+        message0 = title + message
+        if TELEGRAMBOT_TOKEN and TELEGRAMBOT_CHATID:
+            telegrambot_sendmessage(message0)
+        if QMSG_KEY:
+            qmsg_sendmessage(message0)
